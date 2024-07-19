138cc88e502eba75d3eea608db7f74db9869d48c
Peter Kraft <petereliaskraft@gmail.com>
2024-07-19 14:23:24 -0700

    logging

--- dbos_transact/dbos.py
+++ dbos_transact/dbos.py
@@ -1,6 +1,9 @@
+import logging
+
 class DBOS:
     def __init__(self):
-        print("Initializing DBOS!")
+        logger = logging.getLogger(__name__)
+        logger.info("Initializing DBOS!")
 
     def example(self):
         return 0
