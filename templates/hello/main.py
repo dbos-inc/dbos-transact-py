8ca3080e2bb80b8360f82e123260408a4fb9784f
Peter Kraft <petereliaskraft@gmail.com>
2024-07-23 16:18:57 -0700

    test

--- templates/hello/main.py
+++ templates/hello/main.py
@@ -1,3 +1,12 @@
-from dbos_transact import DBOS
+from dbos_transact import DBOS, WorkflowContext
 
 dbos = DBOS()
+
+
+@dbos.workflow()
+def example_workflow(ctx: WorkflowContext, var: str) -> str:
+    return var
+
+
+if __name__ == "__main__":
+    assert example_workflow(dbos.wf_ctx(), "mike") == "mike"
