f581a38027b12c359c5143031097c577bb5a70bc
Qian Li <liqian.cs@gmail.com>
2024-07-25 14:06:46 -0700

    Switch to session for user transactions (#14)

--- dbos_transact/transaction.py
+++ dbos_transact/transaction.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Connection
+from sqlalchemy.orm import Session
 
 
 class TransactionContext:
 
-    def __init__(self, connection: Connection, function_id: int):
-        self.connection = connection
+    def __init__(self, session: Session, function_id: int):
+        self.session = session
         self.function_id = function_id
--- templates/hello/main.py
+++ templates/hello/main.py
@@ -1,3 +1,5 @@
+import sqlalchemy as sa
+
 from dbos_transact import DBOS, WorkflowContext
 from dbos_transact.transaction import TransactionContext
 
@@ -11,8 +13,9 @@ def example_workflow(ctx: WorkflowContext, var: str) -> str:
 
 @dbos.transaction()
 def example_transaction(ctx: TransactionContext, var: str) -> str:
-    return var
+    rows = ctx.session.execute(sa.text("SELECT 1")).fetchall()
+    return var + str(rows[0][0])
 
 
 if __name__ == "__main__":
-    assert example_workflow(dbos.wf_ctx(), "mike") == "mike"
+    assert example_workflow(dbos.wf_ctx(), "mike") == "mike1"
