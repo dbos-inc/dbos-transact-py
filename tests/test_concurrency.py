52efd3598210279f0d7e62834953eaca581139ae
Peter Kraft <petereliaskraft@gmail.com>
2024-08-08 16:23:44 -0700

    test_concurrency

---
+++ tests/test_concurrency.py
@@ -0,0 +1,28 @@
+import random
+import string
+import time
+from concurrent.futures import Future, ThreadPoolExecutor
+from typing import Tuple
+
+from dbos_transact import DBOS, SetWorkflowUUID
+
+
+def test_concurrent_workflows(dbos: DBOS):
+
+    @dbos.workflow()
+    def test_workflow() -> str:
+        time.sleep(1)
+        return DBOS.workflow_id
+
+    def test_thread(id: str) -> str:
+        with SetWorkflowUUID(id):
+            return test_workflow()
+
+    num_threads = 10
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures: list[Tuple[str, Future[str]]] = []
+        for _ in range(num_threads):
+            id = "".join(random.choices(string.ascii_letters + string.digits, k=10))
+            futures.append((id, executor.submit(test_thread, id)))
+        for id, future in futures:
+            assert id == future.result()
