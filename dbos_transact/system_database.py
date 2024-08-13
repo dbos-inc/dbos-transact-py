20443fe7b91bf501f3a108686d70f09f1b9a5d09
Chuck B <chuck.bear@dbos.dev>
2024-08-12 22:53:35 -0400

    Start implementation of WF API

--- dbos_transact/system_database.py
+++ dbos_transact/system_database.py
@@ -73,6 +73,69 @@ class GetEventWorkflowContext(TypedDict):
 
 dbos_null_topic = "__null__topic__"
 
+"""
+
+export async function listWorkflows(config: DBOSConfig, input: GetWorkflowsInput, getRequest: boolean) {
+  const systemDatabase = new PostgresSystemDatabase(config.poolConfig, config.system_database, createLogger() as unknown as GlobalLogger)
+  const workflowUUIDs = (await systemDatabase.getWorkflows(input)).workflowUUIDs.reverse(); // Reverse so most recent entries are printed last
+  const workflowInfos = await Promise.all(workflowUUIDs.map(async (i) => await getWorkflowInfo(systemDatabase, i, getRequest)))
+  await systemDatabase.destroy();
+  return workflowInfos;
+}
+
+async function getWorkflowInfo(systemDatabase: SystemDatabase, workflowUUID: string, getRequest: boolean) {
+  const info = await systemDatabase.getWorkflowStatus(workflowUUID) as WorkflowInformation;
+  info.workflowUUID = workflowUUID;
+  if (info === null) {
+    return {};
+  }
+  const input = await systemDatabase.getWorkflowInputs(workflowUUID);
+  if (input !== null) {
+    info.input = input;
+  }
+  if (info.status === StatusString.SUCCESS) {
+    const result = await systemDatabase.getWorkflowResult(workflowUUID);
+    info.output = result;
+  } else if (info.status === StatusString.ERROR) {
+    const result = await systemDatabase.getWorkflowResult(workflowUUID);
+    info.error = result;
+  }
+  if (!getRequest) {
+    delete info.request;
+  }
+  return info;
+}
+
+export async function getWorkflow(config: DBOSConfig, workflowUUID: string, getRequest: boolean) {
+  const systemDatabase = new PostgresSystemDatabase(config.poolConfig, config.system_database, createLogger() as unknown as GlobalLogger)
+  const info = await getWorkflowInfo(systemDatabase, workflowUUID, getRequest);
+  await systemDatabase.destroy();
+  return info;
+}
+
+// Cancelling a workflow prevents it from being automatically recovered, but active executions are not halted.
+export async function cancelWorkflow(config: DBOSConfig, workflowUUID: string) {
+  const systemDatabase = new PostgresSystemDatabase(config.poolConfig, config.system_database, createLogger() as unknown as GlobalLogger)
+  await systemDatabase.setWorkflowStatus(workflowUUID, StatusString.CANCELLED, false)
+  await systemDatabase.destroy();
+}
+
+export async function reattemptWorkflow(config: DBOSConfig, runtimeConfig: DBOSRuntimeConfig | null, workflowUUID: string, startNewWorkflow: boolean) {
+  const dbosExec = new DBOSExecutor(config);
+  if (runtimeConfig !== null) {
+    await DBOSRuntime.loadClasses(runtimeConfig.entrypoints);
+  }
+  await dbosExec.init();
+  if (!startNewWorkflow) {
+    await dbosExec.systemDatabase.setWorkflowStatus(workflowUUID, StatusString.PENDING, true);
+  }
+  const handle = await dbosExec.executeWorkflowUUID(workflowUUID, startNewWorkflow);
+  const output = await handle.getResult();
+  await dbosExec.destroy();
+  return output;
+}
+"""
+
 
 class SystemDatabase:
 
