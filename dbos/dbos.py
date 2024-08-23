74e3f682c0bb8724c6a508c5ae19cbf60f80b831
Qian Li <liqian.cs@gmail.com>
2024-08-23 12:16:52 -0700

    Update dbos/dbos.py
    Co-authored-by: Peter Kraft <petereliaskraft@gmail.com>

--- dbos/dbos.py
+++ dbos/dbos.py
@@ -635,7 +635,7 @@ class DBOS:
                 if ctx and ctx.is_within_workflow():
                     assert (
                         ctx.is_workflow()
-                    ), "Communicator must be called within a workflow."
+                    ), "Communicators must be called from within workflows"
                     with DBOSAssumeRole(rr):
                         return invoke_comm(*args, **kwargs)
                 else:
