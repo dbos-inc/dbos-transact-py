0a9fd31ea3a9535646c17422e627f9d4e23bc04c
Harry Pierson <harry.pierson@dbos.dev>
2024-08-26 13:43:06 -0700

    update template to use get_dbos_database_url

--- templates/hello/alembic/env.py
+++ templates/hello/alembic/env.py
@@ -1,10 +1,9 @@
-import os
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import URL, engine_from_config, pool
+from sqlalchemy import engine_from_config, pool
 
-from dbos import load_config
+from dbos import get_dbos_database_url
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -15,21 +14,8 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Load DBOS Config and parse the database URL
-dbos_config_path = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "dbos-config.yaml"
-)
-
-dbos_config = load_config(dbos_config_path)
-db_url = URL.create(
-    "postgresql",
-    username=dbos_config["database"]["username"],
-    password=dbos_config["database"]["password"],
-    host=dbos_config["database"]["hostname"],
-    port=dbos_config["database"]["port"],
-    database=dbos_config["database"]["app_db_name"],
-)
-config.set_main_option("sqlalchemy.url", db_url.render_as_string(hide_password=False))
+# programmatically set the sqlalchemy.url field from DBOS Config
+config.set_main_option("sqlalchemy.url", get_dbos_database_url())
 
 # add your model's MetaData object here
 # for 'autogenerate' support
