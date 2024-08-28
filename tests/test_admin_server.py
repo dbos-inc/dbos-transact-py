650f88c7b9220f0d6d4e732e388cca833c5c4a09
Qian Li <qian.li@dbos.dev>
2024-08-27 18:22:09 -0700

    address comments

--- tests/test_admin_server.py
+++ tests/test_admin_server.py
@@ -32,12 +32,12 @@ def test_admin_endpoints(dbos: DBOS) -> None:
     assert "active" in perf_util
     assert "utilization" in perf_util
 
-    # Test GET /dbos-perf again, should be non-zero utilization
-    time.sleep(1)
+    # Test GET /dbos-perf again, should be a valid utilization between 0~1
+    time.sleep(0.2)
     response = requests.get("http://localhost:3001/dbos-perf", timeout=5)
     assert response.status_code == 200
     perf_util = response.json()
-    assert perf_util["utilization"] > 0.0
+    assert perf_util["utilization"] >= 0.0
     assert perf_util["utilization"] <= 1.0
 
     # Test GET not found
