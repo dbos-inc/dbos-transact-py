3213a714cdb8b099eec32b58802b017a9e1a39d5
Harry Pierson <harry.pierson@dbos.dev>
2024-08-29 10:50:07 -0700

    update test_package to specify template

--- tests/test_package.py
+++ tests/test_package.py
@@ -48,7 +48,9 @@ def test_package(build_wheel: str, postgres_db_engine: sa.Engine) -> None:
         subprocess.check_call(["pip", "install", wheel_path], cwd=temp_path, env=venv)
 
         # initalize the app with dbos scaffolding
-        subprocess.check_call(["dbos", "init", "pkgtest"], cwd=temp_path, env=venv)
+        subprocess.check_call(
+            ["dbos", "init", "pkgtest", "--template", "hello"], cwd=temp_path, env=venv
+        )
 
         # Run schema migration
         subprocess.check_call(["dbos", "migrate"], cwd=temp_path, env=venv)
