f78e781493b522f4cc23c39fbb9d68a9e3ea4fff
Peter Kraft <petereliaskraft@gmail.com>
2024-09-06 10:30:41 -0700

    flask

---
+++ tests/test_flask.py
@@ -0,0 +1,43 @@
+import sqlalchemy as sa
+from flask import Flask, jsonify
+
+# Public API
+from dbos import DBOS
+
+
+def test_flask_endpoint(dbos: DBOS) -> None:
+    app = Flask("test_flask_endpoint")
+
+    app.route("/hello")
+
+    @app.route("/endpoint/<var1>/<var2>")
+    def hello(var1, var2):
+        return test_workflow(var1, var2)
+
+    @app.route("/workflow/<var1>/<var2>")
+    @DBOS.workflow()
+    def test_workflow(var1: str, var2: str) -> str:
+        res1 = test_transaction(var1)
+        res2 = test_step(var2)
+        result = res1 + res2
+        return jsonify({"result": result})
+
+    @DBOS.transaction()
+    def test_transaction(var: str) -> str:
+        rows = DBOS.sql_session.execute(sa.text("SELECT 1")).fetchall()
+        return var + str(rows[0][0])
+
+    @DBOS.step()
+    def test_step(var: str) -> str:
+        return var
+
+    app.config["TESTING"] = True
+    client = app.test_client()
+
+    response = client.get("/endpoint/a/b")
+    assert response.status_code == 200
+    assert response.json == {"result": "a1b"}
+
+    response = client.get("/workflow/a/b")
+    assert response.status_code == 200
+    assert response.json == {"result": "a1b"}
