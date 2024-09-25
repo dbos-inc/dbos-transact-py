f9499a15d47923d931c6c67bd05d2f77062450d9
Peter Kraft <petereliaskraft@gmail.com>
2024-09-25 14:23:52 -0700

    Fix Migration (#116)

--- dbos/queue.py
+++ dbos/queue.py
@@ -1,9 +1,9 @@
 import threading
 import time
+import traceback
 from typing import TYPE_CHECKING, Optional
 
 from dbos.core import P, R, _execute_workflow_id, _start_workflow
-from dbos.error import DBOSInitializationError
 
 if TYPE_CHECKING:
     from dbos.dbos import DBOS, Workflow, WorkflowHandle
@@ -31,6 +31,13 @@ def queue_thread(stop_event: threading.Event, dbos: "DBOS") -> None:
     while not stop_event.is_set():
         time.sleep(1)
         for queue_name, queue in dbos._registry.queue_info_map.items():
-            wf_ids = dbos._sys_db.start_queued_workflows(queue_name, queue.concurrency)
-            for id in wf_ids:
-                _execute_workflow_id(dbos, id)
+            try:
+                wf_ids = dbos._sys_db.start_queued_workflows(
+                    queue_name, queue.concurrency
+                )
+                for id in wf_ids:
+                    _execute_workflow_id(dbos, id)
+            except Exception:
+                dbos.logger.warning(
+                    f"Exception encountered in queue thread: {traceback.format_exc()}"
+                )
