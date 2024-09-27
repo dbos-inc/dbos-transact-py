75a1a7a7f334a90f1aa88334b94afa6a83484d31
Chuck B <chuck.bear@dbos.dev>
2024-09-27 08:13:08 -0400

    Update migration for merge and review comment

--- dbos/migrations/versions/d76646551a6c_workflow_queue.py
+++ dbos/migrations/versions/d76646551a6c_workflow_queue.py
@@ -1,8 +1,8 @@
 """workflow_queue
 
-Revision ID: 50f3227f0b4c
-Revises: 50f3227f0b4b
-Create Date: 2024-09-26 12:00:00.0
+Revision ID: d76646551a6c
+Revises: d76646551a6b
+Create Date: 2024-09-27 12:00:00.0
 
 """
 
@@ -12,15 +12,17 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "50f3227f0b4c"
-down_revision: Union[str, None] = "50f3227f0b4b"
+revision: str = "d76646551a6c"
+down_revision: Union[str, None] = "d76646551a6b"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     op.rename_table("job_queue", "workflow_queue", schema="dbos")
+    op.execute("CREATE VIEW dbos.job_queue AS SELECT * FROM dbos.workflow_queue;")
 
 
 def downgrade() -> None:
+    op.execute("DROP VIEW dbos.job_queue;")
     op.rename_table("workflow_queue", "job_queue", schema="dbos")
