2dd148e8073826b2ff6cde41721f301169c94403
Harry Pierson <harry.pierson@dbos.dev>
2024-10-14 13:38:03 -0700

    remove asyncio.run app db

--- dbos/application_database.py
+++ dbos/application_database.py
@@ -114,13 +114,19 @@ class ApplicationDatabase:
     async def record_transaction_output_async(
         session: AsyncSession, output: TransactionResultInternal
     ) -> None:
+        await session.run_sync(ApplicationDatabase.record_transaction_output, output)
+
+    @staticmethod
+    def _record_transaction_error(
+        conn: sa.Connection, output: TransactionResultInternal
+    ) -> None:
         try:
-            await session.execute(
+            conn.execute(
                 pg.insert(ApplicationSchema.transaction_outputs).values(
                     workflow_uuid=output["workflow_uuid"],
                     function_id=output["function_id"],
-                    output=output["output"],
-                    error=None,
+                    output=None,
+                    error=output["error"],
                     txn_id=sa.text("(select pg_current_xact_id_if_assigned()::text)"),
                     txn_snapshot=output["txn_snapshot"],
                     executor_id=(
@@ -134,32 +140,14 @@ class ApplicationDatabase:
             raise
 
     def record_transaction_error(self, output: TransactionResultInternal) -> None:
-        asyncio.run(self.record_transaction_error_async(output))
+        with self.engine.begin() as conn:
+            ApplicationDatabase._record_transaction_error(conn, output)
 
     async def record_transaction_error_async(
         self, output: TransactionResultInternal
     ) -> None:
-        try:
-            async with self.async_engine.begin() as conn:
-                await conn.execute(
-                    pg.insert(ApplicationSchema.transaction_outputs).values(
-                        workflow_uuid=output["workflow_uuid"],
-                        function_id=output["function_id"],
-                        output=None,
-                        error=output["error"],
-                        txn_id=sa.text(
-                            "(select pg_current_xact_id_if_assigned()::text)"
-                        ),
-                        txn_snapshot=output["txn_snapshot"],
-                        executor_id=(
-                            output["executor_id"] if output["executor_id"] else None
-                        ),
-                    )
-                )
-        except DBAPIError as dbapi_error:
-            if dbapi_error.orig.sqlstate == "23505":  # type: ignore
-                raise DBOSWorkflowConflictIDError(output["workflow_uuid"])
-            raise
+        async with self.async_engine.begin() as conn:
+            await conn.run_sync(ApplicationDatabase._record_transaction_error, output)
 
     @staticmethod
     def check_transaction_execution(
@@ -186,22 +174,6 @@ class ApplicationDatabase:
     async def check_transaction_execution_async(
         session: AsyncSession, workflow_uuid: str, function_id: int
     ) -> Optional[RecordedResult]:
-        rows = (
-            await session.execute(
-                sa.select(
-                    ApplicationSchema.transaction_outputs.c.output,
-                    ApplicationSchema.transaction_outputs.c.error,
-                ).where(
-                    ApplicationSchema.transaction_outputs.c.workflow_uuid
-                    == workflow_uuid,
-                    ApplicationSchema.transaction_outputs.c.function_id == function_id,
-                )
-            )
-        ).all()
-        if len(rows) == 0:
-            return None
-        result: RecordedResult = {
-            "output": rows[0][0],
-            "error": rows[0][1],
-        }
-        return result
+        return await session.run_sync(
+            ApplicationDatabase.check_transaction_execution, workflow_uuid, function_id
+        )
