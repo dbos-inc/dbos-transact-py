a59eff92f341dc787f10b23bf9252c12759b683f
Qian Li <qian.li@dbos.dev>
2025-01-17 16:52:09 -0800

    Add hint for potentially non-deterministic workflow

--- tests/test_failures.py
+++ tests/test_failures.py
@@ -10,7 +10,7 @@ from sqlalchemy.exc import InvalidRequestError, OperationalError
 
 # Public API
 from dbos import DBOS, GetWorkflowsInput, Queue, SetWorkflowID
-from dbos._error import DBOSDeadLetterQueueError
+from dbos._error import DBOSDeadLetterQueueError, DBOSException
 from dbos._sys_db import WorkflowStatusString
 
 
@@ -249,3 +249,35 @@ def test_enqueued_dead_letter_queue(dbos: DBOS) -> None:
     assert handle.get_result() == None
     dbos._sys_db.wait_for_buffer_flush()
     assert handle.get_status().status == WorkflowStatusString.SUCCESS.value
+
+
+def test_wfstatus_invalid(dbos: DBOS) -> None:
+    @DBOS.workflow()
+    def regular_workflow() -> str:
+        return "done"
+
+    has_executed = False
+
+    @DBOS.workflow()
+    def non_deterministic_worklow() -> None:
+        nonlocal has_executed
+        handle = dbos.start_workflow(regular_workflow)
+        if not has_executed:
+            # Mock a scenario where the workflow control flow is changed by an external process
+            dbos.set_event("test_event", "value1")
+            has_executed = True
+
+        res = handle.get_result()
+        assert res == "done"
+
+        handle.get_status()
+        return
+
+    wfuuid = str(uuid.uuid4())
+    with SetWorkflowID(wfuuid):
+        non_deterministic_worklow()
+
+    with pytest.raises(DBOSException) as exc_info:
+        with SetWorkflowID(wfuuid):
+            non_deterministic_worklow()
+    assert "Hint: Check if your workflow is deterministic." in str(exc_info.value)
