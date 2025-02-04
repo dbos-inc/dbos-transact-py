88dae2c2afd77a35e3b195fa936caab5301896f4
Qian Li <qian.li@dbos.dev>
2025-02-03 17:21:49 -0800

    Add a test for SQLAlchemy ORM

---
+++ tests/test_sqlalchemy.py
@@ -0,0 +1,60 @@
+import uuid
+
+import sqlalchemy as sa
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+# Public API
+from dbos import DBOS, SetWorkflowID
+
+
+# Declare a SQLAlchemy ORM base class
+class Base(DeclarativeBase):
+    pass
+
+
+# Declare a SQLAlchemy ORM class for accessing the database table.
+class Hello(Base):
+    __tablename__ = "dbos_hello"
+    greet_count: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Hello(greet_count={self.greet_count!r}, name={self.name!r})"
+
+
+def test_simple_workflow(dbos: DBOS) -> None:
+    txn_counter: int = 0
+    assert dbos._app_db_field is not None
+    Base.metadata.drop_all(dbos._app_db_field.engine)
+    Base.metadata.create_all(dbos._app_db_field.engine)
+
+    @DBOS.transaction()
+    def test_transaction(name: str) -> str:
+        new_greeting = Hello(name=name)
+        DBOS.sql_session.add(new_greeting)
+        stmt = (
+            sa.select(Hello)
+            .where(Hello.name == name)
+            .order_by(Hello.greet_count.desc())
+            .limit(1)
+        )
+        row = DBOS.sql_session.scalar(stmt)
+        assert row is not None
+        greet_count = row.greet_count
+        nonlocal txn_counter
+        txn_counter += 1
+        return name + str(greet_count)
+
+    assert test_transaction("alice") == "alice1"
+    assert test_transaction("alice") == "alice2"
+    assert txn_counter == 2
+
+    # Test OAOO
+    wfuuid = str(uuid.uuid4())
+    with SetWorkflowID(wfuuid):
+        assert test_transaction("alice") == "alice3"
+    with SetWorkflowID(wfuuid):
+        assert test_transaction("alice") == "alice3"
+    assert txn_counter == 3  # Only increment once
+
+    Base.metadata.drop_all(dbos._app_db_field.engine)
