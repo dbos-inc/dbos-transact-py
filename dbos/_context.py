ec240d86845a4dbb1362f8859782b6bdf61f5f1a
Peter Kraft <petereliaskraft@gmail.com>
2025-02-25 16:52:14 -0600

    Application Versioning (#215)
    The PR expands the notion of application versions, which were previously
    only used in DBOS Cloud, to support more robust self-hosting of DBOS
    applications.
    
    - When DBOS is launched, it constructs an application version by hashing
    the source of the application's workflows. This version is stable across
    restarts, but changes if the application's code is changed.
    - The application version can be overridden through the
    `DBOS__APPVERSION` environment variable.
    - When recovering workflows, DBOS does not recover workflows belonging
    to different application versions. This prevents code compatibility
    issues.
    - Workflows belonging to different app versions can be manually
    recovered with `dbos workflow resume [workflow-ID]`.

--- dbos/_context.py
+++ dbos/_context.py
@@ -49,7 +49,6 @@ class TracedAttributes(TypedDict, total=False):
 class DBOSContext:
     def __init__(self) -> None:
         self.executor_id = os.environ.get("DBOS__VMID", "local")
-        self.app_version = os.environ.get("DBOS__APPVERSION", "")
         self.app_id = os.environ.get("DBOS__APPID", "")
 
         self.logger = dbos_logger
