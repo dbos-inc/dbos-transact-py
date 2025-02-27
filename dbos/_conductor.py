a11163c72ec2271fb1e8ef2192d3b365a2d6c9a7
Peter Kraft <petereliaskraft@gmail.com>
2025-02-27 12:37:53 -0800

    protocol

--- dbos/_conductor.py
+++ dbos/_conductor.py
@@ -1,6 +1,9 @@
+import json
 import threading
 import time
 import urllib.parse
+from dataclasses import asdict, dataclass
+from enum import Enum
 from typing import TYPE_CHECKING, Optional
 
 from websockets import ConnectionClosed, ConnectionClosedOK
@@ -8,6 +11,36 @@ from websockets.sync.client import ClientConnection, connect
 
 if TYPE_CHECKING:
     from dbos import DBOS
+
+
+class MessageType(str, Enum):
+    EXECUTOR_INFO = "executor_info"
+
+
+@dataclass
+class BaseMessage:
+
+    @classmethod
+    def from_json(cls, json_str):
+        data = json.loads(json_str)
+        kwargs = {k: v for k, v in data.items() if k in cls.__annotations__}
+        return cls(**kwargs)
+
+    def to_json(self):
+        dict_data = asdict(self)
+        return json.dumps(dict_data)
+
+
+@dataclass
+class ExecutorInfoRequest(BaseMessage):
+    type: MessageType = MessageType.EXECUTOR_INFO
+
+
+@dataclass
+class ExecutorInfoResponse(BaseMessage):
+    executor_id: str
+    application_version: str
+    type: MessageType = MessageType.EXECUTOR_INFO
 
 
 class ConductorWebsocket(threading.Thread):
@@ -27,9 +60,14 @@ class ConductorWebsocket(threading.Thread):
         while not self.evt.is_set():
             try:
                 self.websocket = connect(self.url)
-                self.websocket.send("Hello world!")
                 while not self.evt.is_set():
                     message = self.websocket.recv()
+                    base_message = BaseMessage.from_json(message)
+                    type = base_message.type
+                    if type == MessageType.EXECUTOR_INFO.value:
+                        pass
+                    else:
+                        self.dbos.logger.warning(f"Unexpected message type: {type}")
                     print(message)
             except ConnectionClosedOK:
                 self.dbos.logger.info("Conductor connection terminated")
@@ -44,4 +82,5 @@ class ConductorWebsocket(threading.Thread):
                 self.dbos.logger.error(
                     f"Unexpected exception in connection to conductor: {e}"
                 )
-                break
+                time.sleep(1)
+                continue
