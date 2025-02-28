f6160362b8602d289849c492a90d4acc323af0ac
Harry Pierson <harry.pierson@dbos.dev>
2025-02-28 10:25:54 -0800

    don't log during config loading in debug cli command

--- dbos/cli/cli.py
+++ dbos/cli/cli.py
@@ -243,7 +243,7 @@ def reset(
 def debug(
     workflow_id: Annotated[str, typer.Argument(help="Workflow ID to debug")],
 ) -> None:
-    config = load_config()
+    config = load_config(silent=True)
     start = config["runtimeConfig"]["start"]
     if not start:
         typer.echo("No start commands found in 'dbos-config.yaml'")
