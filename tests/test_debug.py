45950bd318c94c08fd9d9dd334425a6ea45839b2
Harry Pierson <harry.pierson@dbos.dev>
2025-03-03 08:53:56 -0800

    test type annotations

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -1,35 +1,35 @@
 from dbos._debug import PythonModule, parse_start_command
 
 
-def test_parse_fast_api_command():
+def test_parse_fast_api_command() -> None:
     command = "fastapi run app/main.py"
     expected = "app/main.py"
     actual = parse_start_command(command)
     assert actual == expected
 
 
-def test_parse_python_command():
+def test_parse_python_command() -> None:
     command = "python app/main.py"
     expected = "app/main.py"
     actual = parse_start_command(command)
     assert actual == expected
 
 
-def test_parse_python3_command():
+def test_parse_python3_command() -> None:
     command = "python3 app/main.py"
     expected = "app/main.py"
     actual = parse_start_command(command)
     assert actual == expected
 
 
-def test_parse_python_module_command():
+def test_parse_python_module_command() -> None:
     command = "python -m some_module"
     actual = parse_start_command(command)
     assert isinstance(actual, PythonModule)
     assert actual.module_name == "some_module"
 
 
-def test_parse_python3_module_command():
+def test_parse_python3_module_command() -> None:
     command = "python3 -m some_module"
     actual = parse_start_command(command)
     assert isinstance(actual, PythonModule)
