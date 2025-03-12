584342d234eb4642dc08e45f104677fb2c5256fb
Harry Pierson <harry.pierson@dbos.dev>
2025-03-11 17:22:28 -0700

    add a comment

--- dbos/_debug.py
+++ dbos/_debug.py
@@ -36,6 +36,7 @@ def debug_workflow(workflow_id: str, entrypoint: Union[str, PythonModule]) -> No
 def parse_start_command(command: str) -> Union[str, PythonModule]:
     match = re.match(r"fastapi\s+run\s+(\.?[\w/]+\.py)", command)
     if match:
+        # Mirror the logic in fastapi's run command by converting the path argument to a module
         mod_data = get_module_data_from_path(Path(match.group(1)))
         sys.path.insert(0, str(mod_data.extra_sys_path))
         return PythonModule(mod_data.module_import_str)
