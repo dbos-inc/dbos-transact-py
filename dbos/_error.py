763b84a868fbbd154e58f63d24a740bdc92388dc
Peter Kraft <petereliaskraft@gmail.com>
2025-03-19 15:32:24 -0500

    Test + Improve Step Retries (#243)
    Improve the error message returned when a step exceeds maximum retries,
    test it rigorously, and fix an issue where the exception object did not
    properly deserialize (because of how jsonpickle/pickle special-cases
    exception serialization).

--- dbos/_error.py
+++ dbos/_error.py
@@ -1,7 +1,7 @@
 """Errors thrown by DBOS."""
 
 from enum import Enum
-from typing import Optional
+from typing import Any, Optional
 
 
 class DBOSException(Exception):
@@ -124,11 +124,17 @@ class DBOSNotAuthorizedError(DBOSException):
 class DBOSMaxStepRetriesExceeded(DBOSException):
     """Exception raised when a step was retried the maximimum number of times without success."""
 
-    def __init__(self) -> None:
+    def __init__(self, step_name: str, max_retries: int) -> None:
+        self.step_name = step_name
+        self.max_retries = max_retries
         super().__init__(
-            "Step reached maximum retries.",
+            f"Step {step_name} has exceeded its maximum of {max_retries} retries",
             dbos_error_code=DBOSErrorCode.MaxStepRetriesExceeded.value,
         )
+
+    def __reduce__(self) -> Any:
+        # Tell jsonpickle how to reconstruct this object
+        return (self.__class__, (self.step_name, self.max_retries))
 
 
 class DBOSWorkflowCancelledError(DBOSException):
--- tests/test_failures.py
+++ tests/test_failures.py
@@ -8,10 +8,15 @@ import sqlalchemy as sa
 from psycopg.errors import SerializationFailure
 from sqlalchemy.exc import InvalidRequestError, OperationalError
 
-# Public API
-from dbos import DBOS, GetWorkflowsInput, SetWorkflowID
-from dbos._error import DBOSDeadLetterQueueError, DBOSException
+from dbos import DBOS, GetWorkflowsInput, Queue, SetWorkflowID
+from dbos._error import (
+    DBOSDeadLetterQueueError,
+    DBOSException,
+    DBOSMaxStepRetriesExceeded,
+)
 from dbos._sys_db import WorkflowStatusString
+
+from .conftest import queue_entries_are_cleaned_up
 
 
 def test_transaction_errors(dbos: DBOS) -> None:
@@ -258,3 +263,64 @@ def test_wfstatus_invalid(dbos: DBOS) -> None:
         with SetWorkflowID(wfuuid):
             non_deterministic_worklow()
     assert "Hint: Check if your workflow is deterministic." in str(exc_info.value)
+
+
+def test_step_retries(dbos: DBOS) -> None:
+    step_counter = 0
+
+    queue = Queue("test-queue")
+    max_attempts = 2
+
+    @DBOS.step(retries_allowed=True, interval_seconds=0, max_attempts=max_attempts)
+    def failing_step() -> None:
+        nonlocal step_counter
+        step_counter += 1
+        raise Exception("fail")
+
+    @DBOS.workflow()
+    def failing_workflow() -> None:
+        failing_step()
+
+    @DBOS.workflow()
+    def enqueue_failing_step() -> None:
+        queue.enqueue(failing_step).get_result()
+
+    error_message = f"Step {failing_step.__name__} has exceeded its maximum of {max_attempts} retries"
+
+    # Test calling the step directly
+    with pytest.raises(DBOSMaxStepRetriesExceeded) as excinfo:
+        failing_step()
+    assert error_message in str(excinfo.value)
+    assert step_counter == max_attempts
+
+    # Test calling the workflow
+    step_counter = 0
+    with pytest.raises(DBOSMaxStepRetriesExceeded) as excinfo:
+        failing_workflow()
+    assert error_message in str(excinfo.value)
+    assert step_counter == max_attempts
+
+    # Test enqueueing the step
+    step_counter = 0
+    handle = queue.enqueue(failing_step)
+    with pytest.raises(DBOSMaxStepRetriesExceeded) as excinfo:
+        handle.get_result()
+    assert error_message in str(excinfo.value)
+    assert step_counter == max_attempts
+
+    # Test enqueuing the workflow
+    step_counter = 0
+    handle = queue.enqueue(failing_workflow)
+    with pytest.raises(DBOSMaxStepRetriesExceeded) as excinfo:
+        handle.get_result()
+    assert error_message in str(excinfo.value)
+    assert step_counter == max_attempts
+
+    # Test enqueuing the step from a workflow
+    step_counter = 0
+    with pytest.raises(DBOSMaxStepRetriesExceeded) as excinfo:
+        enqueue_failing_step()
+    assert error_message in str(excinfo.value)
+    assert step_counter == max_attempts
+
+    assert queue_entries_are_cleaned_up(dbos)
