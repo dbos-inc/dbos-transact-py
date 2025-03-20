b80b7361d667657534cbd4bf38b8d222879172a4
Harry Pierson <harry.pierson@dbos.dev>
2025-03-20 15:07:14 -0700

    add enqueue_async + test

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1013,3 +1013,35 @@ def test_dlq_enqueued_workflows(dbos: DBOS) -> None:
 
     # Verify all queue entries eventually get cleaned up.
     assert queue_entries_are_cleaned_up(dbos)
+
+
+@pytest.mark.asyncio
+async def test_simple_queue_async(dbos: DBOS) -> None:
+    wf_counter: int = 0
+    step_counter: int = 0
+
+    wfid = str(uuid.uuid4())
+
+    @DBOS.workflow()
+    async def test_workflow(var1: str, var2: str) -> str:
+        assert DBOS.workflow_id == wfid
+        nonlocal wf_counter
+        wf_counter += 1
+        var1 = await test_step(var1)
+        return var1 + var2
+
+    @DBOS.step()
+    async def test_step(var: str) -> str:
+        nonlocal step_counter
+        step_counter += 1
+        return var + "d"
+
+    queue = Queue("test_queue")
+
+    with SetWorkflowID(wfid):
+        handle = await queue.enqueue_async(test_workflow, "abc", "123")
+    assert (await handle.get_result()) == "abcd123"
+    with SetWorkflowID(wfid):
+        assert (await test_workflow("abc", "123")) == "abcd123"
+    assert wf_counter == 2
+    assert step_counter == 1
