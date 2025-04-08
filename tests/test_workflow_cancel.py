89fbdcef1806779d71b07eac654697f754b62707
Peter Kraft <petereliaskraft@gmail.com>
2025-04-08 16:15:20 -0700

    tests

--- tests/test_workflow_cancel.py
+++ tests/test_workflow_cancel.py
@@ -1,14 +1,14 @@
 import threading
-import time
 import uuid
 
 import pytest
 
 # Public API
-from dbos import DBOS, ConfigFile, SetWorkflowID
+from dbos import DBOS, Queue, SetWorkflowID
+from tests.conftest import queue_entries_are_cleaned_up
 
 
-def test_cancel_resume(dbos: DBOS, config: ConfigFile) -> None:
+def test_cancel_resume(dbos: DBOS) -> None:
     steps_completed = 0
     workflow_event = threading.Event()
     main_thread_event = threading.Event()
@@ -18,7 +18,7 @@ def test_cancel_resume(dbos: DBOS, config: ConfigFile) -> None:
         nonlocal steps_completed
         steps_completed += 1
 
-    @DBOS.transaction()
+    @DBOS.step()
     def step_two() -> None:
         nonlocal steps_completed
         steps_completed += 1
@@ -35,6 +35,7 @@ def test_cancel_resume(dbos: DBOS, config: ConfigFile) -> None:
     wfid = str(uuid.uuid4())
     with SetWorkflowID(wfid):
         handle = DBOS.start_workflow(simple_workflow)
+    main_thread_event.wait()
     DBOS.cancel_workflow(wfid)
     workflow_event.set()
     with pytest.raises(Exception):
@@ -50,3 +51,51 @@ def test_cancel_resume(dbos: DBOS, config: ConfigFile) -> None:
     handle = DBOS.resume_workflow(wfid)
     assert handle.get_result() == None
     assert steps_completed == 2
+
+
+def test_cancel_resume_queue(dbos: DBOS) -> None:
+    steps_completed = 0
+    workflow_event = threading.Event()
+    main_thread_event = threading.Event()
+
+    queue = Queue("test_queue")
+
+    @DBOS.step()
+    def step_one() -> None:
+        nonlocal steps_completed
+        steps_completed += 1
+
+    @DBOS.step()
+    def step_two() -> None:
+        nonlocal steps_completed
+        steps_completed += 1
+
+    @DBOS.workflow()
+    def simple_workflow() -> None:
+        step_one()
+        main_thread_event.set()
+        workflow_event.wait()
+        step_two()
+
+    # Start the workflow and cancel it.
+    # Verify it stops after step one but before step two
+    wfid = str(uuid.uuid4())
+    with SetWorkflowID(wfid):
+        handle = queue.enqueue(simple_workflow)
+    main_thread_event.wait()
+    DBOS.cancel_workflow(wfid)
+    workflow_event.set()
+    assert steps_completed == 1
+
+    # Resume the workflow. Verify it completes successfully.
+    handle = DBOS.resume_workflow(wfid)
+    assert handle.get_result() == None
+    assert steps_completed == 2
+
+    # Resume the workflow again. Verify it does not run again.
+    handle = DBOS.resume_workflow(wfid)
+    assert handle.get_result() == None
+    assert steps_completed == 2
+
+    # Verify nothing is left on any queue
+    assert queue_entries_are_cleaned_up(dbos)
