e4875faf3fe9a7150edfa928ecbd14d2bd8754c4
Peter Kraft <petereliaskraft@gmail.com>
2025-04-18 13:48:30 -0700

    more test

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -902,6 +902,27 @@ def test_timeout_queue(dbos: DBOS) -> None:
     # Verify the normal workflow succeeds
     normal_handle.get_result()
 
+    # Verify if a parent called with a timeout enqueues a blocked child
+    # the deadline propagates and the child is also cancelled.
+    child_id = str(uuid.uuid4())
+    queue = Queue("regular_queue")
+
+    @DBOS.workflow()
+    def parent_workflow() -> None:
+        with SetWorkflowID(child_id):
+            handle = queue.enqueue(blocking_workflow)
+        handle.get_result()
+
+    with SetWorkflowTimeout(1.0):
+        handle = queue.enqueue(parent_workflow)
+    with pytest.raises(Exception) as exc_info:
+        handle.get_result()
+    assert "was cancelled" in str(exc_info.value)
+
+    with pytest.raises(Exception) as exc_info:
+        DBOS.retrieve_workflow(child_id).get_result()
+    assert "was cancelled" in str(exc_info.value)
+
     # Verify all queue entries eventually get cleaned up.
     assert queue_entries_are_cleaned_up(dbos)
 
