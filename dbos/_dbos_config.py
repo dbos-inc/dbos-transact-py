a53f1305e1ba018b79f30f185504d910e4226c8c
maxdml <maxdml@dbos.dev>
2025-04-22 08:31:41 -0700

    typo

--- dbos/_dbos_config.py
+++ dbos/_dbos_config.py
@@ -315,7 +315,7 @@ def load_config(
     frame_records = inspect.stack()
     called_from_cli = False
     for frame_record in frame_records:
-        if "dbos/cli/cli.py" in frame_record.filename or or "dbos/_dbos.py" in frame_record.filename:
+        if "dbos/cli/cli.py" in frame_record.filename or "dbos/_dbos.py" in frame_record.filename:
             called_from_cli = True
     if not called_from_cli and not silent:
         dbos_logger.warning(
