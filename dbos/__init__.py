90bb3ee6ec16ffe07b9a590c68cb7bb91d4006c4
Peter Kraft <petereliaskraft@gmail.com>
2025-04-23 11:27:13 -0700

    fix

--- dbos/__init__.py
+++ dbos/__init__.py
@@ -1,7 +1,7 @@
 from . import _error as error
 from ._client import DBOSClient, EnqueueOptions
 from ._context import DBOSContextEnsure, DBOSContextSetAuth, SetWorkflowID
-from ._dbos import DBOS, DBOSConfiguredInstance, WorkflowHandle
+from ._dbos import DBOS, DBOSConfiguredInstance, WorkflowHandle, WorkflowHandleAsync
 from ._dbos_config import ConfigFile, DBOSConfig, get_dbos_database_url, load_config
 from ._kafka_message import KafkaMessage
 from ._queue import Queue
@@ -20,7 +20,7 @@ __all__ = [
     "KafkaMessage",
     "SetWorkflowID",
     "WorkflowHandle",
-    "WorkflowStatus",
+    "WorkflowHandleAsync" "WorkflowStatus",
     "WorkflowStatusString",
     "load_config",
     "get_dbos_database_url",
