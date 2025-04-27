2a97acc65a496547ab211700eeb664b9174fd8dc
seanrogan <srogan88@gmail.com>
2025-04-26 23:20:21 -0400

    added statement at line 100 to replace the template name with the given app name. after template name is used to download the template files, the name is changed to the app name so it will be used for the project name when the new files are generated.

--- dbos/cli/_github_init.py
+++ dbos/cli/_github_init.py
@@ -97,6 +97,9 @@ def create_template_from_github(app_name: str, template_name: str) -> None:
         # Create directory if it doesn't exist
         os.makedirs(os.path.dirname(target_path), exist_ok=True)
 
+        # Replace the template name with the given app_name
+        raw_content = raw_content.replace(template_name, app_name)
+
         # Write file with proper permissions
         with open(target_path, "w", encoding="utf-8") as f:
             f.write(raw_content)
