75429901370c480c494bdbad114c2dee95fed2fa
maxdml <maxdml@dbos.dev>
2025-05-01 11:49:40 -0700

    remove the internal ConfigFile struct from public exports

--- dbos/__init__.py
+++ dbos/__init__.py
@@ -8,13 +8,12 @@ from ._context import (
     SetWorkflowTimeout,
 )
 from ._dbos import DBOS, DBOSConfiguredInstance, WorkflowHandle, WorkflowHandleAsync
-from ._dbos_config import ConfigFile, DBOSConfig
+from ._dbos_config import DBOSConfig
 from ._kafka_message import KafkaMessage
 from ._queue import Queue
 from ._sys_db import GetWorkflowsInput, WorkflowStatus, WorkflowStatusString
 
 __all__ = [
-    "ConfigFile",
     "DBOSConfig",
     "DBOS",
     "DBOSClient",
