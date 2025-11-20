b413b365040abcbc2621ce55c0e8868bb2d6eb83
Peter Kraft <petereliaskraft@gmail.com>
2025-11-19 17:22:08 -0800

    done

--- dbos/_conductor/protocol.py
+++ dbos/_conductor/protocol.py
@@ -65,6 +65,7 @@ class ExecutorInfoResponse(BaseMessage):
     application_version: str
     hostname: Optional[str]
     language: Optional[str]
+    transact_version: Optional[str]
     error_message: Optional[str] = None
 
 
