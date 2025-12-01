44128e2c381d6cbb053749d090070cee85f4e3aa
Peter Kraft <petereliaskraft@gmail.com>
2025-12-01 09:59:44 -0800

    improve

--- dbos/_sys_db.py
+++ dbos/_sys_db.py
@@ -405,6 +405,21 @@ class SystemDatabase(ABC):
         import sqlalchemy.dialects.postgresql as pg
         import sqlalchemy.dialects.sqlite as sq
 
+        # Log system database connection information
+        if engine:
+            dbos_logger.info("Using custom system database engine")
+        else:
+            printable_sys_db_url = sa.make_url(system_database_url).render_as_string(
+                hide_password=True
+            )
+            dbos_logger.info(f"DBOS system database URL: {printable_sys_db_url}")
+            if system_database_url.startswith("sqlite"):
+                dbos_logger.info(
+                    f"Using SQLite as a system database. The SQLite system database is for development and testing. PostgreSQL is recommended for production use."
+                )
+            else:
+                dbos_logger.info(f"System database engine parameters: {engine_kwargs}")
+
         self.dialect = sq if system_database_url.startswith("sqlite") else pg
 
         self.serializer = serializer
