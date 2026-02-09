a20fa688213b4b44b0dd66673aed08236e2c5e46
Chuck B <chuck.bear@dbos.dev>
2026-02-09 13:32:56 -0500

    Portable event / stream interop

--- tests/ts_client/portableinvoke.ts
+++ tests/ts_client/portableinvoke.ts
@@ -17,22 +17,40 @@ async function doTest() {
 
     await client.send(wfhs.workflowID, 'm', 'incoming', undefined, { serializationType: 'portable' });
 
-    /*
-    expect(await client.getEvent(wfhs.workflowID, 'defstat', 2)).toStrictEqual({ status: 'Happy' });
-    expect(await client.getEvent(wfhs.workflowID, 'nstat', 2)).toStrictEqual({ status: 'Happy' });
-    expect(await client.getEvent(wfhs.workflowID, 'pstat', 2)).toStrictEqual({ status: 'Happy' });
-    const { value: ddread } = await client.readStream(wfhs.workflowID, 'defstream').next();
-    expect(ddread).toStrictEqual({ stream: 'OhYeah' });
-    const { value: dnread } = await client.readStream(wfhs.workflowID, 'nstream').next();
-    expect(dnread).toStrictEqual({ stream: 'OhYeah' });
+    let deserializationErrs = 0;
+    // TODO console.assert(JSON.stringify(await client.getEvent(wfhs.workflowID, 'defstat', 2)) === JSON.stringify({ status: 'Happy' }));
+    try {
+      console.assert(JSON.stringify(await client.getEvent(wfhs.workflowID, 'nstat', 2)) === JSON.stringify({ status: 'Happy' }));
+    }
+    catch (e) {
+      ++deserializationErrs;
+      if (!(e as Error).message.includes('deserialization type py_pickle is not available')) throw e;
+    }
+    console.assert(JSON.stringify(await client.getEvent(wfhs.workflowID, 'pstat', 2)) === JSON.stringify({ status: 'Happy' }));
+
+    // TODO
+    // const { value: ddread } = await client.readStream(wfhs.workflowID, 'defstream').next();
+    // console.assert(JSON.stringify(ddread) === JSON.stringify({ stream: 'OhYeah' }));
+    try {
+      const { value: dnread } = await client.readStream(wfhs.workflowID, 'nstream').next();
+      console.assert(JSON.stringify(dnread) === JSON.stringify({ stream: 'OYeah' }));
+    }
+    catch (e) {
+      ++deserializationErrs;
+      if (!(e as Error).message.includes('deserialization type py_pickle is not available')) throw e;
+    }  
     const { value: dpread } = await client.readStream(wfhs.workflowID, 'pstream').next();
-    expect(dpread).toStrictEqual({ stream: 'OhYeah' });
-    */
+    console.assert(JSON.stringify(dpread) === JSON.stringify({ stream: 'OhYeah' }));
+
     const rvs = await wfhs.getResult();
     console.assert(rvs === 's-1-k:v@"m"');
+
+    if (deserializationErrs !== 2) {
+      throw new Error("Expected 2 deserialization errors");
+    }
   } finally {
     await client.destroy();
   }
 }
 
-doTest().then(()=>{}).catch(console.error);
+doTest().then(()=>{}).catch((e)=>{console.error(e); process.exit(1)});
