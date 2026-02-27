07d246b3d543424f18f2c76d0428a03f14298592
Peter Kraft <petereliaskraft@gmail.com>
2026-02-27 15:06:19 -0800

    merge

--- dbos/_migration.py
+++ dbos/_migration.py
@@ -316,8 +316,8 @@ CREATE TABLE "{schema}".application_versions (
     version_id TEXT NOT NULL PRIMARY KEY,
     version_name TEXT NOT NULL UNIQUE,
     version_timestamp BIGINT NOT NULL DEFAULT (EXTRACT(epoch FROM now()) * 1000.0)::bigint
-    );
-    """
+);
+"""
 
 
 def get_dbos_migrations(schema: str, use_listen_notify: bool) -> list[str]:
