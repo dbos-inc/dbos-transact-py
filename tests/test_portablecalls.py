40ee4105d39da3203cb1166a9ebfd9107260bcd1
Peter Kraft <petereliaskraft@gmail.com>
2026-03-02 13:12:23 -0800

    fix

--- tests/test_portablecalls.py
+++ tests/test_portablecalls.py
@@ -296,7 +296,7 @@ def test_portable_ser(dbos: DBOS, client: DBOSClient) -> None:
     # Export w/ children
     expwf = dbos._sys_db.export_workflow(wfhd.workflow_id, export_children=True)
     assert 1 == len(expwf)
-    assert 10 == len(expwf[0]["operation_outputs"])
+    assert 11 == len(expwf[0]["operation_outputs"])
     assert wfhd.workflow_id == expwf[0]["workflow_status"]["workflow_uuid"]
     # Delete so it can be reimported
     DBOS.delete_workflow(wfhd.workflow_id, delete_children=True)
