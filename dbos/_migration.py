0440770783b131ef93f8ba4a1d1143bc4cf05e69
Peter Kraft <petereliaskraft@gmail.com>
2026-03-04 13:29:56 -0800

    nit

--- dbos/_migration.py
+++ dbos/_migration.py
@@ -312,7 +312,7 @@ ALTER TABLE "{schema}"."streams" ADD COLUMN "serialization" TEXT DEFAULT NULL;
 def get_dbos_migration_twelve(schema: str) -> str:
     return f"""
 ALTER TABLE "{schema}"."notifications" ADD COLUMN "consumed" BOOLEAN NOT NULL DEFAULT FALSE;
-CREATE INDEX "idx_notifications_unconsumed" ON "{schema}"."notifications" ("destination_uuid", "topic") WHERE consumed = FALSE;
+CREATE INDEX "idx_notifications" ON "{schema}"."notifications" ("destination_uuid", "topic");
 """
 
 
